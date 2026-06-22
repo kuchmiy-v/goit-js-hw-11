7a2d61e87508bf92845b5b5672295bf19185d2d2
kuchmiy_v <wrdvrk@gmail.com>
2026-06-23 00:30:46 +0300

    slBox

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,4 +1,4 @@
-import SLBox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
