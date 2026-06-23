32f52cdd8932e25761a9818fc9c5e04082282c8d
kuchmiy_v <wrdvrk@gmail.com>
2026-06-23 21:31:23 +0300

    fix console err

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,7 +4,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
-const lightbox = new SLBox('.gallery a', {
+const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
